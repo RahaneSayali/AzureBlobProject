f4bdbf2114ed0ed2999556c92e3ad43c519f7a8a
Sayali Rahane <sayalipr15@gmail.com>
2025-11-23 18:19:40 +0000

    added blob service

---
+++ Services/BlobService.cs
@@ -0,0 +1,32 @@
+﻿using AzureBlobProject.Models;
+
+namespace AzureBlobProject.Services
+{
+    public class BlobService : IBlobService
+    {
+        public Task CreateBlob(string name, IFormFile file, string containerName, BlobModel blobModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteBlob(string name, string containerName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<string>> GetAllBlobs(string containerName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BlobModel>> GetAllBlobsWithUri(string containerName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetBlob(string name, string containerName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
